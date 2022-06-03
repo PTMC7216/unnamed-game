fc425c4774906a14f08f0c799cf508f8abf912dc
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-06-03 10:42:06 -0700

    overworld debug cleanup

--- src/states/overworld.py
+++ src/states/overworld.py
@@ -28,19 +28,8 @@ class Overworld(State):
         if self.game.draw_debug:
             pg.draw.rect(self.game.screen, (255, 255, 255),
                          self.game.camera.apply_rect(self.game.player.sprite.rect), 1)
-            for npc in self.game.npcs:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(npc.rect), 1)
-            for entity in self.game.entity:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(entity.rect), 1)
-            for cleaner in self.game.cleaner:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(cleaner.rect), 1)
-            for obstacle in self.game.obstacles:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(obstacle.rect), 1)
-            for closed_door in self.game.closed_doors:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(closed_door.rect), 1)
-            for open_door in self.game.opened_doors:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(open_door.rect), 1)
-            for interactable in self.game.interactables:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(interactable.rect), 1)
-            for item in self.game.items:
-                pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(item.rect), 1)
+            groups = [self.game.npcs, self.game.entity, self.game.cleaner, self.game.obstacles, self.game.closed_doors,
+                      self.game.opened_doors, self.game.interactables, self.game.items]
+            for group in groups:
+                for x in group:
+                    pg.draw.rect(self.game.screen, (255, 255, 255), self.game.camera.apply_rect(x.rect), 1)
