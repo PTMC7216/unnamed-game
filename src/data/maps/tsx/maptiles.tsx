a3889a1bac89cd25f3fc43984bf70a721191f6a2
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-08-15 10:51:48 -0700

    tsx resources & map1 wip

---
+++ src/data/maps/tsx/maptiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="maptiles" tilewidth="32" tileheight="32" tilecount="1984" columns="64">
+ <image source="maptiles.png" width="2048" height="992"/>
+</tileset>
