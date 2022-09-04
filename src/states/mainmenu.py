b2d314e1437159f9a7dd2ec30d25b80c76a3b995
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-09-04 00:41:47 -0700

    Add keybinding options

--- src/states/mainmenu.py
+++ src/states/mainmenu.py
@@ -82,7 +82,7 @@ class OptionsMenu(MainMenu):
         if self.choices[self.index] == 'Audio':
             AudioMenu(self.game).enter_state()
         elif self.choices[self.index] == 'Keybinds':
-            pass
+            KeybindsMenu(self.game).enter_state()
 
 
 class AudioMenu(MainMenu):
@@ -130,6 +130,109 @@ class AudioMenu(MainMenu):
         utils.ptext.draw(f'{round(pg.mixer.music.get_volume(), 1)}',
                          center=(self.pos1['x'] + 30, self.pos1['y']))
         self.draw_selector()
+
+    def transition_state(self):
+        pass
+
+
+class KeybindsMenu(MainMenu):
+    def __init__(self, game):
+        super().__init__(game)
+        self.game = game
+        self.name = 'Keybinds Menu'
+        self.choices = ['UP', 'DOWN', 'LEFT', 'RIGHT', 'SELECT', 'BACK', 'MOD']
+        self.index = 0
+
+        self.pos0 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 20)
+        self.pos1 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 60)
+        self.pos2 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 100)
+        self.pos3 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 140)
+        self.pos4 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 180)
+        self.pos5 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 220)
+        self.pos6 = self.framer.set_pos(self.panel, x=self.pos_adj['x'], y=self.pos_adj['y'] + 260)
+
+        self.position_selector(self.pos0, -110, -2)
+
+    def update(self):
+        self.check_events()
+        self.move_selector()
+        if self.keybool['z']:
+            self.game.select_sound.play()
+            self.transition_state()
+        elif self.keybool['x']:
+            self.game.select_sound.play()
+            self.exit_state()
+        elif self.keybool['esc']:
+            self.game.control = {
+                'up': pg.K_UP,
+                'down': pg.K_DOWN,
+                'left': pg.K_LEFT,
+                'right': pg.K_RIGHT,
+                'select': pg.K_z,
+                'back': pg.K_x,
+                'mod': pg.K_LSHIFT
+            }
+        self.key_reset()
+
+    def render(self):
+        self.game.screen.blit(self.panel['surf'], self.panel['rect'])
+        utils.ptext.draw('Keybinds \nESC to restore defaults', center=(self.title_pos['x'], self.title_pos['y']))
+
+        utils.ptext.draw(self.choices[0], midleft=(self.pos0['x'] - 90, self.pos0['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['up'])}", midleft=(self.pos0['x'] + 40, self.pos0['y']))
+
+        utils.ptext.draw(self.choices[1], midleft=(self.pos1['x'] - 90, self.pos1['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['down'])}", midleft=(self.pos1['x'] + 40, self.pos1['y']))
+
+        utils.ptext.draw(self.choices[2], midleft=(self.pos2['x'] - 90, self.pos2['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['left'])}", midleft=(self.pos2['x'] + 40, self.pos2['y']))
+
+        utils.ptext.draw(self.choices[3], midleft=(self.pos3['x'] - 90, self.pos3['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['right'])}", midleft=(self.pos3['x'] + 40, self.pos3['y']))
+
+        utils.ptext.draw(self.choices[4], midleft=(self.pos4['x'] - 90, self.pos4['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['select'])}", midleft=(self.pos4['x'] + 40, self.pos4['y']))
+
+        utils.ptext.draw(self.choices[5], midleft=(self.pos5['x'] - 90, self.pos5['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['back'])}", midleft=(self.pos5['x'] + 40, self.pos5['y']))
+
+        utils.ptext.draw(self.choices[6], midleft=(self.pos6['x'] - 90, self.pos6['y']))
+        utils.ptext.draw(f"{pg.key.name(self.game.control['mod'])}", midleft=(self.pos6['x'] + 40, self.pos6['y']))
+        self.draw_selector()
+
+    def transition_state(self):
+        Keybinder(self.game, self.choices[self.index]).enter_state()
+
+
+class Keybinder(KeybindsMenu):
+    def __init__(self, game, bind):
+        super().__init__(game)
+        self.bind = bind
+        self.name = 'Keybinder'
+
+    def update(self):
+        self.check_events()
+
+        if self.keybool['esc']:
+            self.exit_state()
+
+        elif self.keydown is not None:
+            for key, value in self.game.control.items():
+                try:
+                    if value == pg.key.key_code(self.keydown):
+                        self.game.control[key] = self.game.control[self.bind.lower()]
+                except ValueError:
+                    return
+
+            self.game.control[self.bind.lower()] = pg.key.key_code(self.keydown)
+            self.exit_state()
+
+        self.key_reset()
+
+    def render(self):
+        self.game.screen.blit(self.panel['surf'], self.panel['rect'])
+        utils.ptext.draw(f"Press any key to bind it to {self.bind}", center=(self.title_pos['x'], self.title_pos['y']))
+        utils.ptext.draw('ESC to cancel', center=(self.title_pos['x'], self.title_pos['y'] + 40))
 
     def transition_state(self):
         pass
