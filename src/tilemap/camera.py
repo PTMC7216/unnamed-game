1e1f56582d7bd51c2b942753f8b218d9611d2e1d
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-09-08 07:51:45 -0700

    Rename Camera rect

--- src/tilemap/camera.py
+++ src/tilemap/camera.py
@@ -7,13 +7,13 @@ class Camera:
 
         self.width = self.game.map.width
         self.height = self.game.map.height
-        self.camera = pg.Rect(0, 0, self.width, self.height)
+        self.rect = pg.Rect(0, 0, self.width, self.height)
 
     def apply_sprite(self, entity):
-        return entity.rect.move(self.camera.topleft)
+        return entity.rect.move(self.rect.topleft)
 
     def apply_rect(self, rect):
-        return rect.move(self.camera.topleft)
+        return rect.move(self.rect.topleft)
 
     def update(self, target):
         x = -target.rect.centerx + int(self.game.screen_res['x'] / 2)
@@ -24,4 +24,4 @@ class Camera:
         x = max(-(self.width - self.game.screen_res['x']), x)
         y = max(-(self.height - self.game.screen_res['y']), y)
 
-        self.camera = pg.Rect(x, y, self.width, self.height)
+        self.rect = pg.Rect(x, y, self.width, self.height)
